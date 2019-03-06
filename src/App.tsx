2a6973733b03b00b4b1dc651e5bfb4eb816147f8
lhirschfeld <liortulip@gmail.com>
2019-03-05 22:01:18 -0500

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Timeline from "./components/timeline/Timeline";
+
+import Event, { Category } from "./api/event";
+import "./App.css";
 
 class App extends Component {
+  readonly state = {
+    events: new Event(
+      "Birth",
+      "I was born in Zefat, Israel, and moved to the United States seven months later.",
+      null,
+      Category.Biography,
+      new Date(1998, 11, 20)
+    ),
+    activeCategory: Category.Biography
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Timeline />
       </div>
     );
   }
